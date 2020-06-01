02decad4427cee4880c1f2a13804858789a73016
DenisLucas <denislucasvalverdesilva@gmail.com>
2020-06-01 12:41:26 -0300

    1.1.0

--- src/Player.gd
+++ src/Player.gd
@@ -1,26 +1,16 @@
-extends Area2D
+extends KinematicBody2D
 
-signal hit
-var speed = 600
-var screen_size
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	screen_size = Vector2(240,160)
-	#$".".hide()
+	pass # Replace with function body.
 
-func _process(delta: float) -> void:
-	var velocidade = Vector2()
-	if Input.is_action_pressed("ui_up"):
-		velocidade.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocidade.y += 1
-	if Input.is_action_pressed("ui_left"):
-		velocidade.x -= 1
-	if Input.is_action_pressed("ui_right"):
-		velocidade.x += 1
-	if velocidade.length() > 0:
-		position = velocidade.normalized() * speed
-	position += velocidade * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
