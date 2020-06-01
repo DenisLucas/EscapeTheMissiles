5bd289948c3059c49b153bbf0ec61e782d59e66d
DenisLucas <denislucasvalverdesilva@gmail.com>
2020-06-01 16:08:43 -0300

    1.1.3

--- misseis.gd
+++ misseis.gd
@@ -3,10 +3,5 @@ extends KinematicBody2D
 func _ready() -> void:
 	$Timer.start()
 
-
-func _on_VisibilityNotifier2D_screen_exited() -> void:
-	queue_free()
-
-
 func _on_Timer_timeout() -> void:
 	$AnimatedSprite.play()
--- src/main.gd
+++ src/main.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
 
-
-
+func _ready() -> void:
+	$MissileTimer.start()
+	
 func game_over() -> void:
 	get_tree().reload_current_scene()
 	print("hit")
+
+
+func _on_MissileTimer_timeout() -> void:
+	$misseis/AnimationPlayer.play("left_to_right")
+	
