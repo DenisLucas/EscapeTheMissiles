d72a124b5600aa591d767b06de7dea907e97a15a
DenisLucas <denislucasvalverdesilva@gmail.com>
2020-06-01 14:31:21 -0300

    1.1.3

--- misseis.gd
+++ misseis.gd
@@ -2,11 +2,7 @@ extends KinematicBody2D
 
 func _ready() -> void:
 	$Timer.start()
-func _physics_process(delta: float) -> void:
-	var velocidade = Vector2(0, -4)
-	velocidade += velocidade * delta
-	print(velocidade)
-	velocidade = move_and_collide(velocidade)
+
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
